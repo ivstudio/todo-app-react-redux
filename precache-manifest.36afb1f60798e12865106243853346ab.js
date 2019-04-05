b789b653849cc4dfb91b7d325622c573d0b26770
iggy@ivstudio.com <iggy@honeybook.com>
2019-04-05 07:40:30 -0400

    Updates

---
+++ precache-manifest.36afb1f60798e12865106243853346ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "33e60ccee56037e9d7e1",
+    "url": "/todo-app-react-redux/static/js/runtime~main.21871b44.js"
+  },
+  {
+    "revision": "fd3f6d2e059e8186551b",
+    "url": "/todo-app-react-redux/static/js/main.8fdaf81c.chunk.js"
+  },
+  {
+    "revision": "9b08dc669d8a887dc913",
+    "url": "/todo-app-react-redux/static/js/2.b04cd6bf.chunk.js"
+  },
+  {
+    "revision": "a50dd1cf696beb6007b088674f0e0085",
+    "url": "/todo-app-react-redux/index.html"
+  }
+];
