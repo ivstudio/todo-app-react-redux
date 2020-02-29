e80d41c5c866c14ec584d303752c3aff399262aa
Iggy Villamar <iggy@ivstudio.com>
2020-02-29 13:32:32 -0500

    Updates

---
+++ precache-manifest.952de89d9e9cc5d7e433aa893d3bfef0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3aad7517daaa2cd91460090e07bacf",
+    "url": "/todo-app-react-redux/index.html"
+  },
+  {
+    "revision": "af8ec04e85945bb2ba3e",
+    "url": "/todo-app-react-redux/static/js/2.494ea246.chunk.js"
+  },
+  {
+    "revision": "8df547a0c05ddff2dc387950fd63ce2c",
+    "url": "/todo-app-react-redux/static/js/2.494ea246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee3add6e32a0ada8737",
+    "url": "/todo-app-react-redux/static/js/main.befb1474.chunk.js"
+  },
+  {
+    "revision": "a844ec162b066bba106b",
+    "url": "/todo-app-react-redux/static/js/runtime-main.7df25907.js"
+  }
+]);
