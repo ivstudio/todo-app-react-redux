e7d7e6a1947d4c75ca5ae4c6bdc7c1d8532e5adc
Ignacio Villamar <iggy@ivstudio.com>
2020-09-19 13:21:22 -0400

    Uppercase Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import Todos from './Todos/Todos';
-import AddTodo from './Todos/AddTodo';
-import FilterTodos from './Todos/FilterTodos';
+import React from "react";
+import Todos from "./Todos/Todos";
+import AddTodo from "./Todos/AddTodo";
+import FilterTodos from "./Todos/FilterTodos";
 
 //material-ui grid
-import Grid from '@material-ui/core/Grid';
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
-	container: {
-		maxWidth: 500,
-		margin: `50px auto`,
-	},
+  container: {
+    maxWidth: 500,
+    margin: `50px auto`
+  }
 };
 
 const App = () => {
-	return (
-		<div style={styles.container}>
-			<Grid container spacing={24}>
-				<Grid item xs={12}>
-					<FilterTodos />
-					<AddTodo />
-				</Grid>
+  return (
+    <div style={styles.container}>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <FilterTodos />
+          <AddTodo />
+        </Grid>
 
-				<Grid item xs={12}>
-					<Todos />
-				</Grid>
-			</Grid>
-		</div>
-	);
+        <Grid item xs={12}>
+          <Todos />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+
+//constants
+import {
+  SHOW_ALL_TODOS,
+  SHOW_COMPLETED_TODOS,
+  SHOW_ACTIVE_TODOS
+} from "../../redux/constants/constants";
+
+//action creators
+import {
+  updateTodo,
+  deleteTodo,
+  toggleDoneTodo
+} from "../../redux/actions/todoActions";
+
+//material-ui components
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+
+//our components
+import TodoItem from "./TodoItem";
+import TodoEmptyState from "./TodoEmptyState";
+
+class Todos extends Component {
+  //render TodoItems
+  renderTodos() {
+    const {
+      todos,
+      deleteTodo,
+      updateTodo,
+      toggleDoneTodo,
+      todosFilter
+    } = this.props;
+    const renderTodoItem = todos.map((todo) => {
+      return (
+        <TodoItem
+          key={todo.id}
+          todoItem={todo}
+          onItemDelete={(id) => deleteTodo(id)}
+          onUpdateTodo={(id, content) => updateTodo(id, content)}
+          onItemToggleComplete={(id) => toggleDoneTodo(id)}
+        />
+      );
+    });
+
+    return (
+      <List>
+        {todos.length > 0 && <Divider />}
+        {!todos.length && <TodoEmptyState activeFilter={todosFilter} />}
+        {renderTodoItem}
+      </List>
+    );
+  }
+
+  render() {
+    return this.renderTodos();
+  }
+}
+
+//filter todos
+const setFilterTodos = (todos, filter) => {
+  switch (filter) {
+    case SHOW_ALL_TODOS:
+      return todos;
+    case SHOW_COMPLETED_TODOS:
+      return todos.filter((t) => t.completed);
+    case SHOW_ACTIVE_TODOS:
+      return todos.filter((t) => !t.completed);
+    default:
+      return todos;
+  }
+};
+
+//return action creators
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      deleteTodo,
+      updateTodo,
+      toggleDoneTodo
+    },
+    dispatch
+  );
+}
+
+//return data our component needs
+function mapStateToProps({ todos, todosFilter }) {
+  return {
+    todos: setFilterTodos(todos, todosFilter),
+    todosFilter
+  };
+}
+
+//export TodoComponent
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
